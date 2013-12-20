a3c22595b0e0c84cad8a136fb59d31b343cbb89f
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2013-12-20 11:17:08 -0500

    Update c9config.sh

--- c9config.sh
+++ c9config.sh
@@ -4,7 +4,6 @@
 
 #echo "" | tee -a ~/bin/bashrc-defaults.sh
 
-echo "alias gitsu='git su'" | tee -a ~/bin/bashrc-defaults.sh
 echo "alias gti='git'" | tee -a ~/bin/bashrc-defaults.sh
 echo "alias server='python -m SimpleHTTPServer $1'" | tee -a ~/bin/bashrc-defaults.sh
 
@@ -12,5 +11,4 @@ echo "wget -O ~/.gitconfig https://raw.github.com/gdumitrescu/dotfiles/master/.g
 echo "wget -O ~/.gitignore https://raw.github.com/gdumitrescu/dotfiles/master/.gitignore" | tee -a ~/bin/bashrc-defaults.sh
 echo "wget -O ~/.gitignore_global https://raw.github.com/gdumitrescu/dotfiles/master/.gitignore_global" | tee -a  ~/bin/bashrc-defaults.sh
 
-cat ~/bin/bashrc-defaults.sh
 source ~/bin/bashrc-defaults.sh
