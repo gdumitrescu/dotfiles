be8bfb878124bbb6f61d79b98317d64fe0466df6
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2014-12-27 15:49:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ wget https://raw.github.com/gdumitrescu/dotfiles/master/tmux/.tmux.conf
 wget https://raw.github.com/gdumitrescu/dotfiles/master/vim/.vimrc
 mkdir -p .vim/colors
 cd .vim/colors
-wget https://raw.github.com/scrooloose/vimfiles/master/colors/railscasts.vim
+wget https://raw.github.com/akitaonrails/vimfiles/master/colors/ir_black.vim
+wget https://raw.github.com/akitaonrails/vimfiles/master/colors/railscasts.vim
 wget https://raw.github.com/akitaonrails/vimfiles/master/colors/railscasts2.vim
+wget https://raw.github.com/akitaonrails/vimfiles/master/colors/zenburn.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
