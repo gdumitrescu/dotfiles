62f5e8d9a030791ccd0c94f68d24097226b8c157
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2019-03-31 16:42:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,7 @@ wget https://raw.github.com/gdumitrescu/dotfiles/master/git/.gitignore_global
 wget https://raw.github.com/gdumitrescu/dotfiles/master/git/.gitconfig
 wget https://raw.github.com/gdumitrescu/dotfiles/master/tmux/.tmux.conf
 wget https://raw.github.com/gdumitrescu/dotfiles/master/vim/.vimrc
-mkdir -p .vim/colors
-cd .vim/colors
-wget https://raw.github.com/akitaonrails/vimfiles/master/colors/ir_black.vim
-wget https://raw.github.com/akitaonrails/vimfiles/master/colors/molokai.vim
-wget https://raw.github.com/akitaonrails/vimfiles/master/colors/railscasts.vim
-wget https://raw.github.com/akitaonrails/vimfiles/master/colors/railscasts2.vim
-wget https://raw.github.com/akitaonrails/vimfiles/master/colors/zenburn.vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 
